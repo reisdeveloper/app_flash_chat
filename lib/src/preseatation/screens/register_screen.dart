337bd7cabcd6401548e474764d4345cf00c9eea6
Jhonatan Santana <reisdeveloper10@gmail.com>
2024-04-02 20:31:22 -0300

    registrer created

--- lib/src/preseatation/screens/register_screen.dart
+++ lib/src/preseatation/screens/register_screen.dart
@@ -1,3 +1,4 @@
+import 'package:app_chat_flash/src/preseatation/widgets/text_field_widghet.dart';
 import 'package:flutter/material.dart';
 
 class ResgistrerScreen extends StatefulWidget {
@@ -10,9 +11,57 @@ class ResgistrerScreen extends StatefulWidget {
 class _RegistrationScreenState extends State<ResgistrerScreen> {
   @override
   Widget build(BuildContext context) {
+    String? password;
+    String? email;
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Resgistrer Screen'),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 15),
+              child: CustomTextField(
+                onTap: () {},
+                onChanged: (value) {
+                  email = value;
+                },
+                labelText: 'Email',
+                hintText: 'youremailexample@gmail.com',
+              ),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 15),
+              child: CustomTextField(
+                onTap: () {},
+                onChanged: (value) {
+                  password = value;
+                },
+                labelText: 'Password',
+                hintText: 'write your password',
+              ),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 15),
+              child: CustomTextField(
+                onTap: () {},
+                onChanged: (value) {
+                  password = value;
+                },
+                labelText: 'Confirm Password',
+                hintText: 'write your password again',
+              ),
+            ),
+            Row(
+              children: [],
+            )
+          ],
+        ),
       ),
     );
   }
