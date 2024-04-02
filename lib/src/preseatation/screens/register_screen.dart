efdaf7d0c515d2d9f4282081422488a5ab2e9df8
Jhonatan Santana <reisdeveloper10@gmail.com>
2024-04-02 20:52:08 -0300

    att screen registrer

--- lib/src/preseatation/screens/register_screen.dart
+++ lib/src/preseatation/screens/register_screen.dart
@@ -9,58 +9,94 @@ class ResgistrerScreen extends StatefulWidget {
 }
 
 class _RegistrationScreenState extends State<ResgistrerScreen> {
+  String? _password;
+  String? _email;
+
   @override
   Widget build(BuildContext context) {
-    String? password;
-    String? email;
+    double widht = MediaQuery.of(context).size.width;
     return Scaffold(
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 15),
-              child: CustomTextField(
-                onTap: () {},
-                onChanged: (value) {
-                  email = value;
-                },
-                labelText: 'Email',
-                hintText: 'exemploseuemail@gmail.com',
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                'Registre-se',
+                style: TextStyle(
+                  fontSize: 40,
+                  fontFamily: 'Poppins',
+                  fontWeight: FontWeight.w600,
+                ),
               ),
-            ),
-            const SizedBox(
-              height: 20,
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 15),
-              child: CustomTextField(
-                onTap: () {},
-                onChanged: (value) {
-                  password = value;
-                },
-                labelText: 'Senha',
-                hintText: 'Digite sua senha',
+              SizedBox(
+                height: 50,
               ),
-            ),
-            const SizedBox(
-              height: 20,
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 15),
-              child: CustomTextField(
-                onTap: () {},
-                onChanged: (value) {
-                  password = value;
-                },
-                labelText: 'Confirme sua senha',
-                hintText: 'Confirme sua senha',
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 30),
+                child: CustomTextField(
+                  onTap: () {},
+                  onChanged: (value) {
+                    _email = value;
+                  },
+                  labelText: 'Email',
+                  hintText: 'exemploseuemail@gmail.com',
+                ),
               ),
-            ),
-            Row(
-              children: [],
-            )
-          ],
+              const SizedBox(
+                height: 20,
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 30),
+                child: CustomTextField(
+                  onTap: () {},
+                  onChanged: (value) {
+                    _password = value;
+                  },
+                  labelText: 'Senha',
+                  hintText: 'Digite sua senha',
+                ),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 30),
+                child: CustomTextField(
+                  onTap: () {},
+                  onChanged: (value) {
+                    _password = value;
+                  },
+                  labelText: 'Confirme sua senha',
+                  hintText: 'Confirme sua senha',
+                ),
+              ),
+              const SizedBox(
+                height: 50,
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20),
+                child: SizedBox(
+                  height: 30,
+                  width: widht,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      Icon(
+                        Icons.person,
+                      ),
+                      Icon(
+                        Icons.person,
+                      ),
+                      Icon(
+                        Icons.person,
+                      )
+                    ],
+                  ),
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
